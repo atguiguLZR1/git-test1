17ca5b79a1825e5cedcf0a97154dac519e922496
atguiguLZR1 <102580576+atguiguLZR1@users.noreply.github.com>
2022-03-30 20:23:22 +0800

    pull commit

--- src/main/java/com/atguigu/git/GidTest.java
+++ src/main/java/com/atguigu/git/GidTest.java
@@ -9,5 +9,6 @@ public class GidTest {
         System.out.println("master");
         System.out.println("hello git4!");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
